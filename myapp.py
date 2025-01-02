bd22c1c9945eb58b8c825d90b3e55e3cfe3cb726
vin082 <vineet082@gmail.com>
2025-01-03 00:29:10 +0530

    Updated myapp.py

--- myapp.py
+++ myapp.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from  crewai import Agent,Task,Crew,Process
 from langchain_openai import ChatOpenAI
 import spacy
